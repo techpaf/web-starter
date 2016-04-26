c6df3e7564f32ed5315e3c5eff8ec1cf83c2adb9
Olivier Destenay <tsbits@MacBook-Pro-de-Olivier-2.local>
2016-04-26 12:59:21 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
+var browserSync = require( 'browser-sync').create();
+var useref = require( 'gulp-useref' );
+var uglify = require( 'gulp-uglify' );
+var gulpIf = require( 'gulp-if' );
+var cssnano = require( 'gulp-cssnano' );
+var del = require( 'del' );
+var imagemin = require('gulp-imagemin');
+
+
+////////////////
+// NANO TASKS //
+////////////////
+
+// Sync task
+gulp.task( 'browserSync', function(){
+  browserSync.init( {
+    server: {
+      baseDir: 'app'
+    },
+  } )
+} );
+
+
+// Compiling SASS task
+gulp.task( 'sass', function(){
+  return gulp.src( 'app/scss/**/*.scss' ) // Gets all files ending with .scss in app/scss and children dirs
+    .pipe( sass() )
+    .pipe( gulp.dest( 'app/css' ) )
+    .pipe( browserSync.reload( {
+      stream: true
+    } ) )
+} );
+
+
+// Deleting all dist content
+gulp.task( 'clean', function() {
+  return del.sync( 'dist' );
+} );
+
+
+// Image optimization
+gulp.task( 'min-images', function() {
+  return gulp.src( 'app/img/*' )
+    .pipe( imagemin( {
+      progressive: true,
+      svgoPlugins: [ { removeViewBox: false } ]
+    } ) )
+    .pipe( gulp.dest( 'dist/img' ) );
+} );
+
+
+/////////////////
+// MACRO TASKS //
+/////////////////
+
+
+// Watch task
+gulp.task( 'watch', ['browserSync'], function(){
+  gulp.watch( 'app/scss/**/*.scss', ['sass'] ); 
+  gulp.watch( 'app/*.html', browserSync.reload ); 
+  gulp.watch( 'app/js/**/*.js', browserSync.reload ); 
+} );
+
+
+// Build task
+gulp.task( 'build', ['clean', 'sass'], function(){
+  gulp.src( 'app/*.html' )
+    .pipe( useref() )
+    .pipe( gulpIf( '*.js', uglify() ) )
+    .pipe( gulpIf( '*.css', cssnano() ) )
+    .pipe( gulp.dest( 'dist' ) );
+
+  // Copy img to dest without optim
+  //gulp.src( 'app/img/**/*' )
+    //.pipe( gulp.dest( 'dist/img/' ) );
+
+  // Copy img to dist and optim
+  gulp.src( 'app/img/**/*' )
+    .pipe( imagemin( {
+      progressive: true,
+      svgoPlugins: [ { removeViewBox: false } ]
+    } ) )
+    .pipe( gulp.dest( 'dist/img/' ) );
+} );
