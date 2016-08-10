7932ac58374b498eab83b2d585fb755ee37553ab
= <desuko@gmail.com>
2016-08-10 22:25:20 +0200

    Updating logo

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-	console.log( 'Ready' );
+	TweenMax.set( $('#bg'), { backgroundImage: 'url(img/00' + Math.ceil(Math.random() * 4) + '.jpg)', opacity: 0 } );	
+	TweenMax.set( $('#content img'), { opacity: 0, y: '250px' } );	
+	TweenMax.set( $('#content p'), { opacity: 0, y: '250px' } );	
+	TweenMax.to( $('#bg'), 0.75, { opacity: 1, ease: Expo.easeOut, delay: 0.5 } );
+	TweenMax.to( $('#content img'), 0.75, { opacity: 1, y: 0, ease: Expo.easeOut, delay: 0.75 } );
+	TweenMax.to( $('#content p'), 0.75, { opacity: 1, y: 0, ease: Expo.easeOut, delay: 0.85 } );
+
 });
