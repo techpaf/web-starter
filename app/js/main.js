6d3b10f5229d4eee94b3b1ae6da93cf6aeb135e8
emoc11 <emoc11@free.fr>
2018-04-06 20:00:49 +0200

    new hand made slider added :)

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function(){
 	console.log('Ready');
+
+	let slider = new LightSlider({
+		debug: true,
+		indicator: true,
+		responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				touch: true,
+				arrows: false,
+				dots: true,
+				adaptHeight: true,
+				keyboard: false,
+				indicator: false
+			}
+		}
+		]
+	});
 });
