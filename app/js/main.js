4103fe3274738aac87e5a7a617758df2830e8e03
tsbits <tsbits@iMac5g-de-tsbits.local>
2018-07-17 11:11:53 +0200

    Update jQuery / base DOM / base JS

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,5 @@
 $(function(){
-	console.log('Ready');
-
-	let slider = new LightSlider({
-		debug: true,
-		indicator: true,
-		responsive: [
-		{
-			breakpoint: 767,
-			settings: {
-				touch: true,
-				arrows: false,
-				dots: true,
-				adaptHeight: true,
-				keyboard: false,
-				indicator: false
-			}
-		}
-		]
-	});
+	console.log('%c🚀 Web Starter ready', 'background: black; color: white; display: block; padding: 5px 10px 5px 10px;');
+	console.log('%c❤️ Create awesome stuff now !', 'background: #fbb03b; color: #fff; display: block; padding: 5px 10px 5px 10px;');
+	console.log('%c👉 Start by editing /app files.', 'background: #fff; color: #333; display: block; padding: 5px 10px 5px 10px;');
 });
